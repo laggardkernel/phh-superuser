be970b3caa6af4add54f64e5dcb0db7267d31654
topjohnwu <topjohnwu@gmail.com>
2016-11-12 02:56:41 +0800

    Fix /sbin binary missing issue

--- common/service.sh
+++ common/service.sh
@@ -30,15 +30,19 @@ $MODDIR/bin/sepolicy-inject --live
 
 # Expose the root path
 log_print "Mounting supath"
-rm -rf /magisk/.core/bin
-mount -o bind $MODDIR/bin /sbin
+rm -rf /magisk/.core/bin $MODDIR/sbin_bind
+mkdir -p $MODDIR/sbin_bind 
+/data/busybox/cp -afc /sbin/. $MODDIR/sbin_bind
+chmod 755 $MODDIR/sbin_bind
+ln -s $MODDIR/bin/* $MODDIR/sbin_bind
+mount -o bind $MODDIR/sbin_bind /sbin
 
 # Run su.d
 for script in $MODDIR/su.d/* ; do
   if [ -f "$script" ]; then
     chmod 755 $script
     log_print "su.d: $script"
-    $script
+    sh $script
   fi
 done
 
